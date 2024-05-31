eede239f6a1dd12c3eeb06c65225d1286c8f8b89
istekhar <amreennaaz888@gmail.com>
2024-06-01 00:11:54 +0530

    first commit

---
+++ Portfolio.js
@@ -0,0 +1,24 @@
+let tablinks=document.getElementsByClassName("tab-links");
+let tabcontents=document.getElementsByClassName("tab-content");
+let sidemenu=document.querySelector("#sidemenu");
+
+
+
+function opentab(tabname){
+    
+        for( let tablink of tablinks){
+            tablink.classList.remove("active-link");
+        }
+        for(let tabcontent of tabcontents){
+            tabcontent.classList.remove("active-tab");
+        }
+event.currentTarget.classList.add("active-link");
+document.getElementById(tabname).classList.add("active-tab");
+
+}
+function openmenu(){
+  sidemenu.style.right="0"; 
+}
+function closemenu(){
+    sidemenu.style.right="-200px"; 
+  }
